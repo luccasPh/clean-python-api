2d9fd1ee4d819aea0b3ac73f427c6e4d97572f48
Lucas Pinheiro <lucas4pinheiro@gmail.com>
2021-02-23 15:19:12 -0300

    Ensure signup controller return 400 if no email is provided

--- app/controllers/signup.py
+++ app/controllers/signup.py
@@ -1,3 +1,8 @@
 class SignUpController:
-    def handle(self, request):
-        return {"status_code": 400, "message": "Missing param: name"}
+    def handle(self, request: dict):
+        data = request.get("body")
+        if not data.get("name"):
+            return {"status_code": 400, "message": "Missing param: name"}
+
+        if not data.get("email"):
+            return {"status_code": 400, "message": "Missing param: email"}
--- tests/controllers/test_signup.py
+++ tests/controllers/test_signup.py
@@ -13,3 +13,17 @@ def test_should_400_if_no_name_provided():
     response = sut.handle(request)
     assert response["status_code"] == 400
     assert response["message"] == "Missing param: name"
+
+
+def test_should_400_if_no_email_provided():
+    sut = SignUpController()
+    request = {
+        "body": {
+            "name": "John Doe",
+            "password": "teste",
+            "password_confirmation": "test",
+        }
+    }
+    response = sut.handle(request)
+    assert response["status_code"] == 400
+    assert response["message"] == "Missing param: email"
