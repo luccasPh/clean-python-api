6ce9cbe970e5f3c60c96dbe7d96914aa9eebcfc3
Lucas Pinheiro <lucas4pinheiro@gmail.com>
2021-02-26 22:03:39 -0300

    Improving test coverage

--- app/presentation/protocols/controller.py
+++ app/presentation/protocols/controller.py
@@ -6,4 +6,4 @@ from .http import Request, Response
 class Controller(ABC):
     @abstractmethod
     def handle(self, request: Request) -> Response:
-        ...
+        """Abstract method for handling a request"""
--- app/presentation/protocols/email_validator.py
+++ app/presentation/protocols/email_validator.py
@@ -4,4 +4,4 @@ from abc import ABC, abstractmethod
 class EmailValidator(ABC):
     @abstractmethod
     def is_valid(self, email: str) -> bool:
-        ...
+        """Abstract method for validating an email"""
