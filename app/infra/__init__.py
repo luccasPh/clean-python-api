fc4b47201d7817e48bf8d2df3001f7d255d1ecfa
Lucas Pinheiro <lucas4pinheiro@gmail.com>
2021-02-28 11:53:08 -0300

    Refactor folders

--- app/infra/__init__.py
+++ app/infra/__init__.py
@@ -1,3 +1,3 @@
 from app.infra.cryptography.bcrypt_adapter import BcryptAdapter
-from app.infra.database.mongodb.account_repo import AccountMongoRepo
+from app.infra.database.mongodb.account.account_repo import AccountMongoRepo
 from app.infra.database.mongodb.mongo_client import get_collection
