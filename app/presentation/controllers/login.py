0717935488f7e6f803264fa8dbeaf2b7c53e7782
Lucas Pinheiro <lucas4pinheiro@gmail.com>
2021-03-01 14:20:14 -0300

    Ensure login controller calls email validator with correct email

--- app/presentation/controllers/login.py
+++ app/presentation/controllers/login.py
@@ -2,13 +2,19 @@ from ..protocols.controller import Controller
 from ..protocols.http import HttpRequest, HttpResponse
 from ..errors.missing_param_error import MissingParamError
 from ..helpers.http_herlper import bad_request
+from ..protocols.email_validator import EmailValidator
 
 
 class LoginController(Controller):
+    def __init__(self, email_validator: EmailValidator):
+        self._email_validator = email_validator
+
     def handle(self, request: HttpRequest) -> HttpResponse:
-        print(request.body)
-        if not request.body.get("email"):
+        data = request.body
+        if not data.get("email"):
             return bad_request(MissingParamError("email"))
 
-        if not request.body.get("password"):
+        if not data.get("password"):
             return bad_request(MissingParamError("password"))
+
+        self._email_validator.is_valid(data.get("email"))
--- tests/presentation/controllers/test_login.py
+++ tests/presentation/controllers/test_login.py
@@ -1,11 +1,18 @@
 import pytest
+from mock import patch, MagicMock
 
-from app.presentation import LoginController, HttpRequest
+from app.presentation import LoginController, HttpRequest, EmailValidator
+
+
+class EmailValidatorStub(EmailValidator):
+    def is_valid(self, email: str) -> bool:
+        return True
 
 
 @pytest.fixture
 def sut():
-    sut = LoginController()
+    email_validator_stub = EmailValidatorStub()
+    sut = LoginController(email_validator_stub)
     yield sut
 
 
@@ -21,3 +28,12 @@ def test_should_400_if_no_password_provided(sut: LoginController):
     response = sut.handle(request)
     assert response.status_code == 400
     assert response.body["message"] == "Missing param: password"
+
+
+@patch.object(EmailValidatorStub, "is_valid")
+def test_should_call_email_validator_correct_value(
+    mock_is_valid: MagicMock, sut: LoginController
+):
+    request = HttpRequest(body=dict(email="email@example.com", password="password"))
+    sut.handle(request)
+    mock_is_valid.assert_called_with("email@example.com")
