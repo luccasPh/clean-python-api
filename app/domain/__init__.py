5d79a8108e631afe2c2729a9d401afb18900c0af
Lucas Pinheiro <lucas4pinheiro@gmail.com>
2021-03-02 12:21:07 -0300

    Remove duplicate import

--- app/domain/__init__.py
+++ app/domain/__init__.py
@@ -1,4 +1,3 @@
 from app.domain.model.account import AccountModel
-from app.domain.usecases.add_account import AddAccount
-from app.domain.usecases.add_account import AddAccountModel
+from app.domain.usecases.add_account import AddAccount, AddAccountModel
 from app.domain.usecases.authentication import Authentication
