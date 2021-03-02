fb42df69640ba1ae0f55e885ccf731d1a105b5a0
Lucas Pinheiro <lucas4pinheiro@gmail.com>
2021-03-02 14:22:59 -0300

    Ensure compare fields validation returns a invalid param error if validation fails

---
+++ tests/presentation/helpers/validators/test_compare_fields_validation.py
@@ -0,0 +1,25 @@
+import pytest
+
+from app.presentation import CompareFieldsValidation
+
+
+@pytest.fixture
+def sut():
+    sut = CompareFieldsValidation("field_1", "field_2")
+    yield sut
+
+
+def test_should_return_invalid_param_error_if_validation_fails(
+    sut: CompareFieldsValidation,
+):
+    input = dict(field_1="value", field_2="wrong_value")
+    is_error = sut.validate(input)
+    assert is_error.args[0] == "Invalid param: field_2"
+
+
+# def test_should_not_return_if_validation_succeeds(
+#     sut: CompareFieldsValidation,
+# ):
+#     input = dict(any_field="any_field")
+#     is_error = sut.validate(input)
+#     assert not is_error
