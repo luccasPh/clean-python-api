bb608203e97991cd6261e6c4c24eac031385d792
Lucas Pinheiro <lucas4pinheiro@gmail.com>
2021-03-02 14:51:53 -0300

    Ensure validation composite returns the first error if many validation fails

--- tests/presentation/helpers/validators/test_validation_composite.py
+++ tests/presentation/helpers/validators/test_validation_composite.py
@@ -12,7 +12,7 @@ class ValidationStub(Validation):
 @pytest.fixture
 def sut():
     validation_stub = ValidationStub()
-    sut = ValidationComposite([validation_stub])
+    sut = ValidationComposite([validation_stub, validation_stub, validation_stub])
     yield sut
 
 
@@ -25,3 +25,29 @@ def test_should_return_an_error_if_any_validation_fails(
     input = dict(field="field")
     is_error = sut.validate(input)
     assert is_error.args[0] == "field error"
+
+
+@patch.object(ValidationStub, "validate")
+def test_should_return_the_first_error_if_many_validation_fails(
+    mock_validate: MagicMock,
+    sut: ValidationComposite,
+):
+    mock_validate.side_effect = [
+        Exception("first error"),
+        Exception("second error"),
+        Exception("third error"),
+    ]
+    input = dict(field="field")
+
+    with pytest.raises(Exception) as excinfo:
+        sut.validate(input)
+
+    assert excinfo.value.args[0] == "first error"
+
+
+# def test_should_not_return_if_all_validation_succeeds(
+#     sut: ValidationComposite,
+# ):
+#     input = dict(field="field")
+#     is_error = sut.validate(input)
+#     assert not is_error
