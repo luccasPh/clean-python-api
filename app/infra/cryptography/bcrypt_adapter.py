16e04d0e99e18c8d919ad81f3a9688a1cce2090d
Lucas Pinheiro <lucas4pinheiro@gmail.com>
2021-03-02 19:43:00 -0300

    Ensure bcrypt adapter calls bcrypt compare with correct values

--- app/infra/cryptography/bcrypt_adapter.py
+++ app/infra/cryptography/bcrypt_adapter.py
@@ -1,12 +1,15 @@
-from bcrypt import hashpw
+from bcrypt import hashpw, checkpw
 
-from app.data import Hasher
+from app.data import Hasher, HashComparer
 
 
-class BcryptAdapter(Hasher):
+class BcryptAdapter(Hasher, HashComparer):
     def __init__(self, salt: bytes):
         self._salt = salt
 
     def hash(self, value: str) -> str:
         hash = hashpw(value.encode("utf-8"), self._salt)
         return hash.decode("utf-8")
+
+    def compare(self, value: str, hash: str) -> bool:
+        return checkpw(value.encode("utf-8"), hash.encode("utf-8"))
--- tests/infra/cryptography/test_bcrypt_adapter.py
+++ tests/infra/cryptography/test_bcrypt_adapter.py
@@ -13,7 +13,7 @@ def sut():
 
 
 @patch("app.infra.cryptography.bcrypt_adapter.hashpw")
-def test_should_call_bcrypt_with_correct_values(
+def test_should_call_hash_with_correct_values(
     mock_hashpw: MagicMock, sut: BcryptAdapter
 ):
     value = "any_value"
@@ -22,7 +22,7 @@ def test_should_call_bcrypt_with_correct_values(
 
 
 @patch("app.infra.cryptography.bcrypt_adapter.hashpw")
-def test_should_return_hash(mock_hashpw: MagicMock, sut: BcryptAdapter):
+def test_should_return_hash_on_hash_success(mock_hashpw: MagicMock, sut: BcryptAdapter):
     value = "hash"
     mock_hashpw.return_value = value.encode("utf-8")
     hash = sut.hash("any_value")
@@ -35,3 +35,13 @@ def test_should_raise_if_bcrypt_raise(mock_hashpw: MagicMock, sut: BcryptAdapter
     with pytest.raises(Exception) as excinfo:
         assert sut.hash("any_value")
     assert type(excinfo.value) is Exception
+
+
+@patch("app.infra.cryptography.bcrypt_adapter.checkpw")
+def test_should_call_compare_with_correct_values(
+    mock_checkpw: MagicMock, sut: BcryptAdapter
+):
+    value = "any_value"
+    hash = "any_hash"
+    sut.compare(value, hash)
+    mock_checkpw.assert_called_with(value.encode("utf-8"), hash.encode("utf-8"))
