33e0d644158292195bc4e26e1bf4d6e5894e8748
Lucas Pinheiro <lucas4pinheiro@gmail.com>
2021-03-03 11:58:42 -0300

    Ensure account mongo repo returns an account on load by email success

--- app/infra/database/mongodb/account/account_repo.py
+++ app/infra/database/mongodb/account/account_repo.py
@@ -2,10 +2,10 @@ from pymongo.collection import Collection
 from dataclasses import asdict
 
 from app.domain import AccountModel, AddAccountModel
-from app.data import AddAccountRepo
+from app.data import AddAccountRepo, LoadAccountByEmailRepo
 
 
-class AccountMongoRepo(AddAccountRepo):
+class AccountMongoRepo(AddAccountRepo, LoadAccountByEmailRepo):
     def __init__(self, account_collection: Collection):
         self._account_collection = account_collection
 
@@ -14,5 +14,10 @@ class AccountMongoRepo(AddAccountRepo):
         account_data["hashed_password"] = account_data.pop("password")
         obj_id = self._account_collection.insert_one(account_data).inserted_id
         account = self._account_collection.find_one({"_id": obj_id})
+        account["id"] = str(account.pop("_id"))
+        return AccountModel(**account)
+
+    def load_by_email(self, email: str) -> AccountModel:
+        account = self._account_collection.find_one({"email": email})
         account["id"] = str(account.pop("_id"))
         return AccountModel(**account)
--- tests/infra/database/mongodb/test_account_repo.py
+++ tests/infra/database/mongodb/test_account_repo.py
@@ -5,14 +5,15 @@ from app.infra import AccountMongoRepo
 from app.domain import AddAccountModel
 
 
+MOCK_COLLECTION = mongomock.MongoClient().db.collection
+
+
 @pytest.fixture
 def sut():
-    mock_collection = mongomock.MongoClient().db.collection
-    sut = AccountMongoRepo(mock_collection)
-    yield sut
+    yield AccountMongoRepo(MOCK_COLLECTION)
 
 
-def test_should_return_an_account(sut: AccountMongoRepo):
+def test_should_return_an_account_on_add_success(sut: AccountMongoRepo):
     data = AddAccountModel(
         name="valid_name", email="valid_email@example.com", password="valid_password"
     )
@@ -23,3 +24,19 @@ def test_should_return_an_account(sut: AccountMongoRepo):
     assert account.name == "valid_name"
     assert account.email == "valid_email@example.com"
     assert account.hashed_password == "valid_password"
+
+
+def test_should_return_an_account_on_load_by_email_success(sut: AccountMongoRepo):
+    MOCK_COLLECTION.insert_one(
+        dict(
+            name="valid_name",
+            email="valid_email@example.com",
+            hashed_password="hashed_password",
+        )
+    )
+    account = sut.load_by_email("valid_email@example.com")
+    assert account
+    assert account.id
+    assert account.name == "valid_name"
+    assert account.email == "valid_email@example.com"
+    assert account.hashed_password == "hashed_password"
