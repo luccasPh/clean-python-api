ba482593606d5d1e84cfb7ad5c7d71fa8e732cce
Lucas Pinheiro <lucas4pinheiro@gmail.com>
2021-03-04 19:23:49 -0300

    Ensure account repository return none on add

--- app/infra/database/mongodb/account/account_repo.py
+++ app/infra/database/mongodb/account/account_repo.py
@@ -10,13 +10,10 @@ class AccountMongoRepo(AddAccountRepo, LoadAccountByEmailRepo, UpdateAccessToken
     def __init__(self, account_collection: Collection):
         self._account_collection = account_collection
 
-    def add(self, data: AddAccountModel) -> AccountModel:
+    def add(self, data: AddAccountModel):
         account_data = asdict(data)
         account_data["hashed_password"] = account_data.pop("password")
-        obj_id = self._account_collection.insert_one(account_data).inserted_id
-        account = self._account_collection.find_one({"_id": obj_id})
-        account["id"] = str(account.pop("_id"))
-        return AccountModel(**account)
+        self._account_collection.insert_one(account_data).inserted_id
 
     def load_by_email(self, email: str) -> AccountModel:
         account = self._account_collection.find_one({"email": email})
