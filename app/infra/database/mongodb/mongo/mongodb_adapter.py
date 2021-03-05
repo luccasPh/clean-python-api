0015bd098ed49b50c76fb4561a63bb67b0b7b6c1
Lucas Pinheiro <lucas4pinheiro@gmail.com>
2021-03-05 13:20:32 -0300

    Add mongodb adapater

---
+++ app/infra/database/mongodb/mongo/mongodb_adapter.py
@@ -0,0 +1,11 @@
+from pymongo.collection import Collection
+
+from app.validations import DbSearchByField
+
+
+class MongoDbAdapter(DbSearchByField):
+    def __init__(self, collection: Collection):
+        self._collection = collection
+
+    def search_by_field(self, field: str, value: str) -> bool:
+        ...
