ba8142074c7821a7daf0994295b82592939cd2d6
Lucas Pinheiro <lucas4pinheiro@gmail.com>
2021-03-05 13:05:21 -0300

    Refactor email validation

--- app/validations/validators/email_validation.py
+++ app/validations/validators/email_validation.py
@@ -1,5 +1,4 @@
-from app.presentation import Validation, InvalidParamError, EmailInUseError
-from ..protocols.email_availability import EmailAvailability
+from app.presentation import Validation, InvalidParamError
 from ..protocols.email_validator import EmailValidator
 
 
@@ -8,20 +7,14 @@ class EmailValidation(Validation):
         self,
         field_name: str,
         email_validator: EmailValidator,
-        email_availability: EmailAvailability,
     ):
         self.field_name = field_name
         self._email_validator = email_validator
-        self._email_availability = email_availability
 
     def validate(self, input):
         is_valid = self._email_validator.is_valid(input[self.field_name])
         if not is_valid:
             return InvalidParamError(self.field_name)
-
-        account = self._email_availability.load_by_email(input[self.field_name])
-        if account:
-            return EmailInUseError()
 
     def __str__(self):
         return f"EmailValidation: {self.field_name}"
--- tests/validations/test_email_validation.py
+++ tests/validations/test_email_validation.py
@@ -2,7 +2,7 @@ import pytest
 import mongomock
 from mock import patch, MagicMock
 
-from app.validations import EmailValidation, EmailValidator, EmailAvailability
+from app.validations import EmailValidation, EmailValidator
 
 
 class EmailValidatorStub(EmailValidator):
@@ -10,18 +10,10 @@ class EmailValidatorStub(EmailValidator):
         return True
 
 
-class EmailAvailabilityStub(EmailAvailability):
-    def load_by_email(self, email: str):
-        return None
-
-
 @pytest.fixture
 def sut():
     email_validator_stub = EmailValidatorStub()
-    load_account_by_email_repo_stub = EmailAvailabilityStub()
-    sut = EmailValidation(
-        "email", email_validator_stub, load_account_by_email_repo_stub
-    )
+    sut = EmailValidation("email", email_validator_stub)
     yield sut
 
 
@@ -62,42 +54,3 @@ def test_should_raise_if_email_validator_raise_exception(
     with pytest.raises(Exception) as excinfo:
         assert sut.validate(input)
     assert type(excinfo.value) is Exception
-
-
-@patch.object(EmailAvailabilityStub, "load_by_email")
-def test_should_call_load_accout_by_email_correct_value(
-    mock_load_by_email: MagicMock, sut: EmailValidation
-):
-    input = {
-        "email": "test@example.com",
-    }
-    sut.validate(input)
-    mock_load_by_email.assert_called_with("test@example.com")
-
-
-@patch("app.main.decorators.log.get_collection")
-@patch.object(EmailAvailabilityStub, "load_by_email")
-def test_should_raise_if_load_account_by_email_raise_exception(
-    mock_load_by_email: MagicMock, mock_get_collection: MagicMock, sut: EmailValidation
-):
-    mock_load_by_email.side_effect = Exception("Error on matrix")
-    mock_get_collection.return_value = mongomock.MongoClient().db.collection
-    input = {
-        "email": "test@example.com",
-    }
-
-    with pytest.raises(Exception) as excinfo:
-        assert sut.validate(input)
-    assert type(excinfo.value) is Exception
-
-
-@patch.object(EmailAvailabilityStub, "load_by_email")
-def test_should_return_email_in_used_if_email_used_provided(
-    mock_load_by_email: MagicMock, sut: EmailValidation
-):
-    mock_load_by_email.return_value = "Account"
-    input = {
-        "email": "email_used@example.com",
-    }
-    result = sut.validate(input)
-    assert result.args[0] == "The received email is already in use"
