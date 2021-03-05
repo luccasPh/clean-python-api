ef419eb6ffbcbbe45ef846cbc4ae40741dd15ddc
Lucas Pinheiro <lucas4pinheiro@gmail.com>
2021-03-05 13:53:36 -0300

    Ensure raise exception if collection find raise

--- tests/infra/database/mongodb/test_mongodb_adapter.py
+++ tests/infra/database/mongodb/test_mongodb_adapter.py
@@ -14,8 +14,18 @@ def sut():
 
 
 @patch.object(MOCK_COLLECTION, "find")
-def test_should_calls_collection_find_correct_value(
+def test_should_calls_collection_find_correct_values(
     mock_find: MagicMock, sut: MongoDbAdapter
 ):
     sut.search_by_field("email", "test@example.com")
     mock_find.assert_called_with({"email": "test@example.com"})
+
+
+@patch.object(MOCK_COLLECTION, "find")
+def test_should_raise_exception_if_collection_find_raise(
+    mock_find: MagicMock, sut: MongoDbAdapter
+):
+    mock_find.side_effect = Exception()
+    with pytest.raises(Exception) as excinfo:
+        assert sut.search_by_field("email", "test@example.com")
+    assert type(excinfo.value) is Exception
