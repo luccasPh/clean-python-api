1c4a697ecd8f697529907a334b96145ea39e0451
Lucas Pinheiro <lucas4pinheiro@gmail.com>
2021-03-06 13:56:22 -0300

    Ensure db load account by token returns none if decrypter return none

--- tests/data/usecases/test_db_load_account_by_token.py
+++ tests/data/usecases/test_db_load_account_by_token.py
@@ -19,5 +19,14 @@ def sut():
 def test_should_call_decrypt_with_correct_value(
     mock_decrypt: MagicMock, sut: DbLoadAccountByToken
 ):
-    sut.load_by_token("any_token")
+    sut.load_by_token("any_token", "any_role")
     mock_decrypt.assert_called_with("any_token")
+
+
+@patch.object(DecrypterStub, "decrypt")
+def test_should_return_null_if_decrypter_returns_null(
+    mock_decrypt: MagicMock, sut: DbLoadAccountByToken
+):
+    mock_decrypt.return_value = None
+    account = sut.load_by_token("any_token", "any_role")
+    assert not account
