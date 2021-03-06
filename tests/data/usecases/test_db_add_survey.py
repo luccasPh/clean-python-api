5eb3591bc13845577720d1583d21525d9c90d97f
Lucas Pinheiro <lucas4pinheiro@gmail.com>
2021-03-06 09:49:32 -0300

    Ensure db add survey raise exception if add survey repo raise

--- tests/data/usecases/test_db_add_survey.py
+++ tests/data/usecases/test_db_add_survey.py
@@ -26,3 +26,17 @@ def test_should_call_add_on_add_survey_repo_correct_values(
     )
     sut.add(survey_data)
     mock_add.assert_called_with(survey_data)
+
+
+@patch.object(AddSurveyRepoStub, "add")
+def test_should_raise_exception_if_add_survey_repo_raise(
+    mock_add: MagicMock, sut: DbAddSurvey
+):
+    mock_add.side_effect = Exception()
+    data = AddSurveyModel(
+        question="any_question",
+        answers=[SurveyAnswer(image="any_image", answer="any_answer")],
+    )
+    with pytest.raises(Exception) as excinfo:
+        assert sut.add(data)
+    assert type(excinfo.value) is Exception
