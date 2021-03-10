152ff1e686a4f0af68accf9aec84eac4221822f4
Lucas Pinheiro <lucas4pinheiro@gmail.com>
2021-03-10 19:13:20 -0300

    Add save survey result usecase

---
+++ app/domain/usecases/save_survey_result.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+
+from ..model.survey_result import SurveyResultModel
+
+
+@dataclass
+class SaveSurveyResultModel:
+    survey_id: str
+    account_id: str
+    answer: str
+
+
+class SaveSurveyResult(ABC):
+    @abstractmethod
+    def save(self, data: SaveSurveyResultModel) -> SurveyResultModel:
+        """Abstract method for save an survey result"""
