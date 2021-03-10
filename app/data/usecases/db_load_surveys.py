5a5e1e35be5e77e3e23d086ecd25791f4c407b8f
Lucas Pinheiro <lucas4pinheiro@gmail.com>
2021-03-09 21:16:02 -0300

    Ensure db load surveys returns a list of surveys on success

--- app/data/usecases/db_load_surveys.py
+++ app/data/usecases/db_load_surveys.py
@@ -1,9 +1,13 @@
+from typing import Union
+
 from app.data import LoadSurveysRepo
+from app.domain import LoadSurveys, SurveyModel
 
 
-class DbLoadSurveys:
+class DbLoadSurveys(LoadSurveys):
     def __init__(self, load_surveys_repo: LoadSurveysRepo):
         self._load_surveys_repo = load_surveys_repo
 
-    def load(self):
-        self._load_surveys_repo.load_all()
+    def load(self) -> Union[list[SurveyModel], None]:
+        surveys = self._load_surveys_repo.load_all()
+        return surveys
--- tests/data/usecases/test_db_load_surveys.py
+++ tests/data/usecases/test_db_load_surveys.py
@@ -2,6 +2,7 @@ import pytest
 from typing import Union
 from datetime import datetime
 from mock import patch, MagicMock
+from freezegun import freeze_time
 
 from app.data import DbLoadSurveys, LoadSurveysRepo
 from app.domain import SurveyModel
@@ -9,19 +10,23 @@ from app.domain import SurveyModel
 
 class LoadSurveysRepoStub(LoadSurveysRepo):
     def load_all(self) -> Union[list[SurveyModel], None]:
-        fake_survey_1 = dict(
-            id="any_id",
-            question="any_question",
-            answers=[dict(image="any_image", answer="any_answer")],
-            date=datetime.utcnow(),
-        )
-        fake_survey_2 = dict(
-            id="other_id",
-            question="other_question",
-            answers=[dict(image="other_image", answer="other_answer")],
-            date=datetime.utcnow(),
-        )
-        return [SurveyModel(**fake_survey_1), SurveyModel(**fake_survey_2)]
+        return make_surveys()
+
+
+def make_surveys() -> list[SurveyModel]:
+    fake_survey_1 = dict(
+        id="any_id",
+        question="any_question",
+        answers=[dict(image="any_image", answer="any_answer")],
+        date=datetime.utcnow(),
+    )
+    fake_survey_2 = dict(
+        id="other_id",
+        question="other_question",
+        answers=[dict(image="other_image", answer="other_answer")],
+        date=datetime.utcnow(),
+    )
+    return [SurveyModel(**fake_survey_1), SurveyModel(**fake_survey_2)]
 
 
 @pytest.fixture
@@ -34,3 +39,9 @@ def sut():
 def test_should_call_load_surveys_repo(mock_load_all: MagicMock, sut: DbLoadSurveys):
     sut.load()
     mock_load_all.assert_called()
+
+
+@freeze_time("2021-03-09")
+def test_should_return_a_list_of_surveys_on_success(sut: DbLoadSurveys):
+    surveys = sut.load()
+    assert surveys == make_surveys()
