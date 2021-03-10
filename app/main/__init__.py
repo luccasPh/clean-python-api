a1673943d6d90fb80185cca6d25450ea0950d74f
Lucas Pinheiro <lucas4pinheiro@gmail.com>
2021-03-10 12:17:08 -0300

    Move add survey to a sigle folder

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,3 +1,5 @@
 from app.main.factories.signup.signup_validation import make_signup_validation
 from app.main.factories.login.login_validation import make_login_validation
-from app.main.factories.survey.add_survey_validation import make_add_survey_validation
+from app.main.factories.add_survey.add_survey_validation import (
+    make_add_survey_validation,
+)
