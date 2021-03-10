ab5d91d7f1d0d82a6fdd2d0529920e4f97d938bf
Lucas Pinheiro <lucas4pinheiro@gmail.com>
2021-03-10 20:48:45 -0300

    Ensure db save survey result raise exception if save survey result repo raise

--- tests/data/usecases/test_db_save_survey_result.py
+++ tests/data/usecases/test_db_save_survey_result.py
@@ -35,3 +35,16 @@ def test_should_call_save_on_save_survey_result_repo_correct_values(
     )
     sut.save(survey_result)
     mock_save.assert_called_with(survey_result)
+
+
+@patch.object(SaveSurveyResultRepoStub, "save")
+def test_should_raise_exception_if_load_survey_repo_raise(
+    mock_save: MagicMock, sut: DbSaveSurveyResult
+):
+    mock_save.side_effect = Exception("Error on matrix")
+    survey_result = SaveSurveyResultModel(
+        survey_id="any_survey_id", account_id="any_account_id", answer="any_answer"
+    )
+    with pytest.raises(Exception) as excinfo:
+        assert sut.save(survey_result)
+    assert type(excinfo.value) is Exception
