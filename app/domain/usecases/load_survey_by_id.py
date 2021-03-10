4c9aa39de804c2764a61529c711b98bc0a4c438b
Lucas Pinheiro <lucas4pinheiro@gmail.com>
2021-03-10 19:12:53 -0300

    Add load survey by id usecase

---
+++ app/domain/usecases/load_survey_by_id.py
@@ -0,0 +1,10 @@
+from abc import ABC, abstractmethod
+from typing import Union
+
+from ..model.survey import SurveyModel
+
+
+class LoadSurveyById(ABC):
+    @abstractmethod
+    def loadById(self, id: str) -> Union[list[SurveyModel], None]:
+        """Abstract method for load a survey by id"""
