b815e2186b444ef6c298b0233332dec616c50cbe
Lucas Pinheiro <lucas4pinheiro@gmail.com>
2021-03-10 19:16:21 -0300

    Add imports

--- app/domain/__init__.py
+++ app/domain/__init__.py
@@ -1,7 +1,10 @@
 from app.domain.model.account import AccountModel
 from app.domain.model.survey import SurveyModel, SurveyAnswerModel
+from app.domain.model.survey_result import SurveyResultModel
 from app.domain.usecases.add_account import AddAccount, AddAccountModel
 from app.domain.usecases.authentication import Authentication, AuthenticationModel
 from app.domain.usecases.add_survey import AddSurvey, AddSurveyModel
 from app.domain.usecases.load_account_by_token import LoadAccountByToken
 from app.domain.usecases.load_surveys import LoadSurveys
+from app.domain.usecases.save_survey_result import SaveSurveyResult
+from app.domain.usecases.load_survey_by_id import LoadSurveyById
