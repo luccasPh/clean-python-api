790fadf28ba00a6cf0f20804fc55f8d18ba692a3
Lucas Pinheiro <lucas4pinheiro@gmail.com>
2021-03-10 12:17:19 -0300

    fixed route bug

--- app/main/adapters/fastapi_route_adapter.py
+++ app/main/adapters/fastapi_route_adapter.py
@@ -1,12 +1,16 @@
+import json
+
 from fastapi import Request, Response
 
 from app.presentation import Controller, HttpRequest
 
 
 async def adpter_route(request: Request, response: Response, controller: Controller):
-    body = await request.json()
+    body = await request.body()
     headers = request.headers
-    http_response = controller.handle(HttpRequest(headers=headers, body=body))
+    http_response = controller.handle(
+        HttpRequest(headers=headers, body=json.loads(body or "{}"))
+    )
     response.status_code = http_response.status_code
     if http_response.status_code == 204:
         return Response(status_code=204)
