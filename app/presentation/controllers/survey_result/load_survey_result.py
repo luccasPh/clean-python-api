ee9926c446f45e0c27b1fac9d9ce03c01f30ab1c
Lucas Pinheiro <lucas4pinheiro@gmail.com>
2021-03-17 14:51:17 -0300

    Ensure load survey result controller return 500 if load survey by id raise exception

--- app/presentation/controllers/survey_result/load_survey_result.py
+++ app/presentation/controllers/survey_result/load_survey_result.py
@@ -1,8 +1,11 @@
+import traceback
+
 from app.domain import LoadSurveyById
-from ...helpers.http.http_herlper import forbidden
+from ...helpers.http.http_herlper import forbidden, server_error
 from ...protocols.http import HttpRequest, HttpResponse
 from ...protocols.controller import Controller
 from ...errors.invalid_param_error import InvalidParamError
+from ...errors.server_error import ServerError
 
 
 class LoadSurveyResultController(Controller):
@@ -10,7 +13,10 @@ class LoadSurveyResultController(Controller):
         self._load_survey_by_id = load_survey_by_id
 
     def handle(self, request: HttpRequest) -> HttpResponse:
-        survey_id = request.params.get("survey_id")
-        survey = self._load_survey_by_id.load_by_id(survey_id)
-        if not survey:
-            return forbidden(InvalidParamError("survey_id"))
+        try:
+            survey_id = request.params.get("survey_id")
+            survey = self._load_survey_by_id.load_by_id(survey_id)
+            if not survey:
+                return forbidden(InvalidParamError("survey_id"))
+        except Exception:
+            return server_error(ServerError(), traceback.format_exc())
--- tests/presentation/controllers/survey_result/test_load_survey_result copy.py
+++ tests/presentation/controllers/survey_result/test_load_survey_result copy.py
@@ -40,3 +40,13 @@ def test_should_403_if_load_survey_by_id_returns_none(
     http_response = sut.handle(HttpRequest(params=dict(survey_id="any_id")))
     assert http_response.status_code == 403
     assert http_response.body["message"] == "Invalid param: survey_id"
+
+
+@patch.object(LoadSurveyByIdStub, "load_by_id")
+def test_should_500_if_load_survey_by_id_raise_exception(
+    mock_load_by_id: MagicMock, sut: LoadSurveyResultController
+):
+    mock_load_by_id.side_effect = Exception("Error on matrix")
+    http_response = sut.handle(HttpRequest(params=dict(survey_id="any_id")))
+    assert http_response.status_code == 500
+    assert http_response.body["message"] == "Internal server error"
