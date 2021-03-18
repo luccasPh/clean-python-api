1cf297a8606ccade99ea020a3fb965c2c4313297
Lucas Pinheiro <lucas4pinheiro@gmail.com>
2021-03-18 17:24:08 -0300

    Setup signup schema validation

--- app/main/factories/signup/signup_validation.py
+++ app/main/factories/signup/signup_validation.py
@@ -1,3 +1,5 @@
+from schema import Schema, And
+
 from app.infra import EmailValidatorAdapter
 from app.infra import MongoDbAdapter, get_collection
 from app.validations import (
@@ -10,12 +12,25 @@ from app.validations import (
 
 
 def make_signup_validation():
+    signup_schema = Schema(
+        dict(
+            name=And(str, len, error="Invalid key: 'name'"),
+            email=And(str, len, error="Invalid key: 'email'"),
+            password=And(
+                And(str, len, error="Invalid key: 'password'"),
+                And(
+                    lambda s: len(s) > 5,
+                    error="Invalid key: 'password minimum 6 characters'",
+                ),
+            ),
+            password_confirmation=And(
+                And(str, len, error="Invalid key: 'password_confirmation'"),
+            ),
+        )
+    )
     return ValidationComposite(
         [
-            RequiredFieldsValidation("name"),
-            RequiredFieldsValidation("email"),
-            RequiredFieldsValidation("password"),
-            RequiredFieldsValidation("password_confirmation"),
+            RequiredFieldsValidation(signup_schema, "signup"),
             CompareFieldsValidation("password", "password_confirmation"),
             EmailValidation(
                 "email", EmailValidatorAdapter(check_mx=False, skip_smtp=True)
--- tests/main/factories/test_signup_validation.py
+++ tests/main/factories/test_signup_validation.py
@@ -4,10 +4,7 @@ from app.main import make_signup_validation
 def test_should_call_validation_composite_with_all_validations():
     validation_composite = make_signup_validation()
     expected = [
-        "RequiredFieldValidation: name",
-        "RequiredFieldValidation: email",
-        "RequiredFieldValidation: password",
-        "RequiredFieldValidation: password_confirmation",
+        "RequiredFieldValidation: signup",
         "CompareFieldsValidation: password -> password_confirmation",
         "EmailValidation: email",
         "UniqueFieldValidation: email",
