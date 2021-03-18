92a9b4fd599cd07282c296bf689f6e172bf8f0c3
Lucas Pinheiro <lucas4pinheiro@gmail.com>
2021-03-18 17:08:27 -0300

    Ensure required field validation returns a invalid param erro if validation fails

--- tests/validations/test_required_field_validation.py
+++ tests/validations/test_required_field_validation.py
@@ -29,6 +29,14 @@ def test_should_return_missing_param_error_if_validation_fails(
     assert is_error.args[0] == "Missing param: 'field'"
 
 
+def test_should_return_invalid_param_error_if_validation_fails(
+    sut: RequiredFieldValidation,
+):
+    input = dict(field="")
+    is_error = sut.validate(input)
+    assert is_error.args[0] == "Invalid param: 'field'"
+
+
 def test_should_not_return_if_validation_succeeds(
     sut: RequiredFieldValidation,
 ):
